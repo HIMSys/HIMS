098b011d472b2f0e5301fd78d3ece56668389698
Paul Antonau <paul.antonau@gmail.com>
2017-09-04 00:14:10 +0300

    Added the IUnitOfWork interface to DAL

---
+++ HIMS/HIMS.DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HIMS.EntityClasses;
+
+namespace HIMS.DAL.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<TestTable> Tests { get; }
+        void Save();
+    }
+}
