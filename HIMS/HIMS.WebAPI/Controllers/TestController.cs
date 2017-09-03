c351d1341e924c4190466aff3c2b16a429d089a0
Paul Antonau <paul.antonau@gmail.com>
2017-09-03 22:25:44 +0300

    Added the TestController to the WebAPI

---
+++ HIMS/HIMS.WebAPI/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HIMS.WebAPI.Controllers
+{
+    public class TestController : ApiController
+    {
+        public IHttpActionResult GetTest()
+        {
+            return Ok( new { Test = "This is test" } );
+        }
+    }
+}
