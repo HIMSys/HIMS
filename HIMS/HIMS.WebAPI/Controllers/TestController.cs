18c0931ef0423073ee3a914f0f655cd258eb06f4
Paul Antonau <paul.antonau@gmail.com>
2017-09-04 23:58:56 +0300

    Added the TestViewModel and updated the TestController

--- HIMS/HIMS.WebAPI/Controllers/TestController.cs
+++ HIMS/HIMS.WebAPI/Controllers/TestController.cs
@@ -4,14 +4,28 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using AutoMapper;
+using HIMS.BusinessLogic.DTO;
+using HIMS.Interfaces.BusinessLogic;
+using HIMS.WebAPI.Models;
 
 namespace HIMS.WebAPI.Controllers
 {
     public class TestController : ApiController
     {
+        ITestService _testService;
+
+        public TestController(ITestService testService)
+        {
+            _testService = testService;
+        }
+
         public IHttpActionResult GetTest()
         {
-            return Ok( new { Test = "This is test" } );
+            IEnumerable<TestTransferModel> testDtos = _testService.GetTests();
+            Mapper.Initialize(cfg => cfg.CreateMap<TestTransferModel, TestViewModel>());
+            var phones = Mapper.Map<IEnumerable<TestTransferModel>, List<TestViewModel>>(testDtos);
+            return Ok(phones);
         }
     }
 }
---
+++ HIMS/HIMS.WebAPI/Models/TestViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HIMS.WebAPI.Models
+{
+    public class TestViewModel
+    {
+        public int TestId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
