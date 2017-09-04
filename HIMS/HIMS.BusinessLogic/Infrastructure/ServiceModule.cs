87a75443c467bb69f4508b422507a79514076f7c
Paul Antonau <paul.antonau@gmail.com>
2017-09-04 23:44:50 +0300

    Added the NinjectModule

---
+++ HIMS/HIMS.BusinessLogic/Infrastructure/ServiceModule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HIMS.Data.Interfaces;
+using HIMS.Data.Repositories;
+using Ninject.Modules;
+
+namespace HIMS.BusinessLogic.Infrastructure
+{
+    public class ServiceModule : NinjectModule
+    {
+        private string connectionString;
+        public ServiceModule(string connection)
+        {
+            connectionString = connection;
+        }
+        public override void Load()
+        {
+            Bind<IUnitOfWork>().To<EFUnitOfWork>().WithConstructorArgument(connectionString);
+        }
+    }
+}
