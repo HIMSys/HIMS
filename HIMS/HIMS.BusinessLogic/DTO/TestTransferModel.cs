e00f0e5b198028eee9f1005e197740084676f37d
Paul Antonau <paul.antonau@gmail.com>
2017-09-04 22:31:30 +0300

    Added the BLL with the DTO for the Test Model

---
+++ HIMS/HIMS.BusinessLogic/DTO/TestTransferModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIMS.BusinessLogic.DTO
+{
+    public class TestTransferModel
+    {
+        public int TestId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
