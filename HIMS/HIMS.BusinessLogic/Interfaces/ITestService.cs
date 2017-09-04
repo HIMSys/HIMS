673fdfe64d4fb60a7c207bf19e827b5a44506728
Paul Antonau <paul.antonau@gmail.com>
2017-09-04 22:59:55 +0300

    Added the BusinessModels, and Infrastructure, and Interfaces folders with all needed testing features

---
+++ HIMS/HIMS.BusinessLogic/Interfaces/ITestService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HIMS.BusinessLogic.DTO;
+
+namespace HIMS.Interfaces.BusinessLogic
+{
+    public interface ITestService
+    {
+        void SaveTest(TestTransferModel testTM);
+        TestTransferModel GetTest(int? id);
+        IEnumerable<TestTransferModel> GetTests();
+        void Dispose();
+    }
+}
