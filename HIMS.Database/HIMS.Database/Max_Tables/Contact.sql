39f55b9dec796dd949dbb9b1e1d49e40d64c5d21
Maximsan <maximuzz@yandex.ru>
2017-10-01 14:05:54 +0300

    removed identity, added unique to contact table, added progress table

--- HIMS.Database/HIMS.Database/Max_Tables/Contact.sql
+++ HIMS.Database/HIMS.Database/Max_Tables/Contact.sql
@@ -1,10 +1,11 @@
 ﻿CREATE TABLE [dbo].[Contact]
 (
-	[Id] INT IDENTITY(1,1) NOT NULL,
+	[Id] INT NOT NULL,
 	[AddressId] INT NOT NULL,
 	[PhoneId] INT NOT NULL,
 
 	CONSTRAINT PK_Contact PRIMARY KEY ([Id]),
+	CONSTRAINT QC_Contact_To_Phone UNIQUE (PhoneId),
 	CONSTRAINT FK_Contact_To_Address FOREIGN KEY (AddressId) REFERENCES [dbo].[Address](Id),
 	CONSTRAINT FK_Contact_To_Phone FOREIGN KEY (PhoneId) REFERENCES [dbo].[Phone](Id)
 )
---
+++ HIMS.Database/HIMS.Database/Max_Tables/Progress.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Progress]
+(
+	[Id] INT NOT NULL,
+	[Date] NVARCHAR(50) NOT NULL,
+	[Note] NVARCHAR(MAX) NOT NULL,
+	[TimeStamp] TIMESTAMP NOT NULL, 
+	[SubtaskId] INT NOT NULL, 
+
+	CONSTRAINT PK_Progress PRIMARY KEY ([Id]),
+	CONSTRAINT FK_Progress_To_ProgressSubtask FOREIGN KEY (SubtaskId) REFERENCES ProgressSubtask(Id)
+)
