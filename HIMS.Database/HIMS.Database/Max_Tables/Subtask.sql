40daaa0000f13b1e1a669ac9ccc5b3c19997e579
Maximsan <maximuzz@yandex.ru>
2017-10-01 14:25:04 +0300

    Added subtask table to database

---
+++ HIMS.Database/HIMS.Database/Max_Tables/Subtask.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Subtask]
+(
+	[Id] INT NOT NULL,
+	[Name] NVARCHAR(50) NOT NULL,
+	[Description] NVARCHAR(MAX),
+	[StartDate] NVARCHAR(50),
+	[EndDate] NVARCHAR(50),
+	[OriginalEstimate] INT,
+
+	CONSTRAINT PK_Subtask PRIMARY KEY ([Id])
+)
