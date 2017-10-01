69cbbd5d2384551ab79d9ce4c99e73aadd26de69
Maximsan <maximuzz@yandex.ru>
2017-10-01 12:01:31 +0300

    Added contact table to database

---
+++ HIMS.Database/HIMS.Database/Max_Tables/Contact.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Contact]
+(
+	[Id] INT IDENTITY(1,1) NOT NULL,
+	[AddressId] INT NOT NULL,
+	[PhoneId] INT NOT NULL,
+
+	CONSTRAINT PK_Contact PRIMARY KEY ([Id]),
+	CONSTRAINT FK_Contact_To_Address FOREIGN KEY (AddressId) REFERENCES [dbo].[Address](Id),
+	CONSTRAINT FK_Contact_To_Phone FOREIGN KEY (PhoneId) REFERENCES [dbo].[Phone](Id)
+)
