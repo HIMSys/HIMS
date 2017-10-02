4698916598253129a233d9b669ac0d37ee0748eb
Yauheni Kokhna <kokhna.yauheni@gmail.com>
2017-10-02 23:02:14 +0300

    Renamed Test to KnowlegeTest, renamed Test_For_Introduction to Test, included the Test in proj

--- HIMS.Database/HIMS.Database/Tables/Test.sql
+++ HIMS.Database/HIMS.Database/Tables/Test.sql
@@ -1,12 +1,8 @@
 ﻿CREATE TABLE [dbo].[Test]
 (
-	[Id] INT NOT NULL, 
-    [TestTypeId] INT NOT NULL, 
-    [Result] FLOAT NOT NULL, 
-    [TestDate] DATE NOT NULL
-	
-	CONSTRAINT [PK_Test] PRIMARY KEY ([Id]), 
-    CONSTRAINT [FK_Test_To_TestType] FOREIGN KEY ([TestTypeId]) REFERENCES [TestType]([Id]) ON DELETE SET NULL,
-	
-	
+	[TestId] INT IDENTITY(1,1) NOT NULL,
+	[Name] NVARCHAR(25) NOT NULL,
+	[Description] NVARCHAR(255) NULL
+
+	CONSTRAINT [PK_Test] PRIMARY KEY ([TestId])
 )
