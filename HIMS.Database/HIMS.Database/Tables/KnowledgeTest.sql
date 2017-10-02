66b39c507ed403d19ac1cc1645de50dd37408a1d
Yauheni Kokhna <kokhna.yauheni@gmail.com>
2017-10-02 23:03:55 +0300

    Renamed PK and FK in the KnowledgeTest table

--- HIMS.Database/HIMS.Database/Tables/KnowledgeTest.sql
+++ HIMS.Database/HIMS.Database/Tables/KnowledgeTest.sql
@@ -5,8 +5,8 @@
     [Result] FLOAT NOT NULL, 
     [TestDate] DATE NOT NULL
 	
-	CONSTRAINT [PK_Test] PRIMARY KEY ([Id]), 
-    CONSTRAINT [FK_Test_To_TestType] FOREIGN KEY ([TestTypeId]) REFERENCES [TestType]([Id]) ON DELETE SET NULL,
+	CONSTRAINT [PK_KnowledgeTest] PRIMARY KEY ([Id]), 
+    CONSTRAINT [FK_KnowledgeTest_To_TestType] FOREIGN KEY ([TestTypeId]) REFERENCES [TestType]([Id]) ON DELETE SET NULL,
 	
 	
 )
