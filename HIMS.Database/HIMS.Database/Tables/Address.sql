6b1efd47bc1ea8bd38def02d0263d2b20b97911d
Yauheni Kokhna <kokhna.yauheni@gmail.com>
2017-10-02 22:44:16 +0300

    Deleted TestType, renamed old Test to Test_For_Introduction (excluded from proj), added Test, fixed another small issues

--- HIMS.Database/HIMS.Database/Tables/Address.sql
+++ HIMS.Database/HIMS.Database/Tables/Address.sql
@@ -4,8 +4,8 @@
     [City] NVARCHAR(20) NOT NULL, 
     [Street] NVARCHAR(30) NOT NULL, 
     [HouseNo] INT NULL, 
-    [FlatNo] INT NULL DEFAULT NULL, -- some houses have no flat number 
-    [ZipCode] NVARCHAR(10) NULL DEFAULT NULL
+    [FlatNo] INT NULL , -- some houses have only one flat
+    [ZipCode] NVARCHAR(10) NULL 
 
 	CONSTRAINT [PK_Address] PRIMARY KEY ([Id]), 	
 )
--- HIMS.Database/HIMS.Database/Tables/TestType.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[TestType]
-(
-	[Id] INT NOT NULL, 
-    [Name] NCHAR(25) NOT NULL, 
-    [Description] NCHAR(255) NULL DEFAULT NULL 
-)
