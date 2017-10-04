14b4aeb150724ce5c28fd0492c8b90be07ffdeaf
ArtiomTapunov <Tapunov_AT@tut.by>
2017-10-04 08:12:39 +0300

    Added cross DB trigger

--- HIMS.Database/HIMS.Database/Tables/UserProfile.sql
+++ HIMS.Database/HIMS.Database/Tables/UserProfile.sql
@@ -8,7 +8,29 @@
 	[Birthday] DATE NOT NULL,
 	[KnowledgeAssessmentId] INT NOT NULL
 
-	CONSTRAINT [FK_UserSecurity_To_UserProfile] FOREIGN KEY ([Id]) REFERENCES [OtherDataBse].[UserSecurity]([Id]),
 	CONSTRAINT [PK_UserProfile] PRIMARY KEY ([Id]),
 	CONSTRAINT [FK_UserProfile_To_KnowledgeAssessment] FOREIGN KEY ([KnowledgeAssessmentId]) REFERENCES [KnowledgeAssessment]([Id]),
 )
+
+GO
+
+CREATE TRIGGER [dbo].[CrossDB_FK_Trigger]
+    ON [dbo].[UserProfile]
+    FOR INSERT, UPDATE
+    AS
+    BEGIN
+        SET NoCount ON
+
+		IF EXISTS (
+        SELECT * FROM INSERTED AS I
+        WHERE NOT EXISTS (
+            SELECT *
+            FROM [OtherDatabase].[dbo].[UserSecurity] AS A
+            WHERE I.Id = A.Id
+			)
+		)
+		BEGIN
+			RAISERROR('Lookup Value Not Found, Insert Failed',16,1);
+			ROLLBACK;
+		END
+    END
