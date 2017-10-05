62dca9cd33d4184205b9c6b728f3331c697f1df6
Maximsan <maximuzz@yandex.ru>
2017-10-05 17:12:25 +0300

    Modified UserTask table

--- HIMS.Database/HIMS.Database/Tables/UserTask.sql
+++ HIMS.Database/HIMS.Database/Tables/UserTask.sql
@@ -1,15 +1,12 @@
 ﻿CREATE TABLE [dbo].[UserTask]
 (
 	[Id] INT NOT NULL,
-    [TaskId] INT NOT NULL, 
+    [TaskId] INT NULL, --if task was deleted, for example by mistake, but users' tasks were not
     [UserId] INT NOT NULL, 
-    [TodoId] INT NOT NULL, 
-    [ProgressId] INT NOT NULL, 
-    [TimeStamp] TIMESTAMP NOT NULL
+	[isActive] BIT NULL DEFAULT 0,
+	[isDone] BIT NULL DEFAULT 0,
 
 	CONSTRAINT [PK_UserTask] PRIMARY KEY ([Id]), 
-    CONSTRAINT [FK_UserTask_To_UserProfile] FOREIGN KEY ([UserId]) REFERENCES [UserProfile]([Id]) ON DELETE SET NULL, 
-    CONSTRAINT [FK_UserTask_To_Task] FOREIGN KEY ([TaskId]) REFERENCES [Task]([Id]) ON DELETE SET NULL, 
-    CONSTRAINT [FK_UserTask_To_Todo] FOREIGN KEY ([TodoId]) REFERENCES [Todo]([Id]) ON DELETE SET NULL, 
-    CONSTRAINT [FK_UserTask_To_Progress] FOREIGN KEY ([ProgressId]) REFERENCES [Progress]([Id]) ON DELETE SET NULL
+    CONSTRAINT [FK_UserTask_To_UserProfile] FOREIGN KEY ([UserId]) REFERENCES [UserProfile]([Id]) ON DELETE CASCADE ON UPDATE CASCADE, 
+    CONSTRAINT [FK_UserTask_To_Task] FOREIGN KEY ([TaskId]) REFERENCES [Task]([Id]) ON DELETE SET NULL 
 )
