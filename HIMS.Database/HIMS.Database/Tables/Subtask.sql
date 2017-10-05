7bc3aa6a1ceb47a5b8d5679707d5efb6eb6f26ee
Maximsan <maximuzz@yandex.ru>
2017-10-05 17:26:24 +0300

    Modified Subtask table

--- HIMS.Database/HIMS.Database/Tables/Subtask.sql
+++ HIMS.Database/HIMS.Database/Tables/Subtask.sql
@@ -1,11 +1,17 @@
 ﻿CREATE TABLE [dbo].[Subtask]
 (
 	[Id] INT IDENTITY(1,1) NOT NULL,
+	[UserTaskId] INT NULL,
 	[Name] NVARCHAR(50) NOT NULL,
 	[Description] NVARCHAR(MAX), --optional
-	[StartDate] Date,            --optional
-	[EndDate] Date,              --optional
-	[OriginalEstimate] INT,      --optional, if the task cannot be divided in parts
+	[StartDate] Date NULL DEFAULT NULL,            --optional
+	[EndDate] Date NULL DEFAULT NULL,              --optional
+	[OriginalEstimate] INT NULL DEFAULT NULL,      --optional, if the task cannot be divided in parts
+	[SpendedTime] INT NULL DEFAULT NULL,
+	[isActive] BIT NULL DEFAULT 0,
+	[isDone] BIT NULL DEFAULT 0,
+	[TimeStamp] TIMESTAMP NOT NULL,
 
-	CONSTRAINT PK_Subtask PRIMARY KEY ([Id])
+	CONSTRAINT PK_Subtask PRIMARY KEY ([Id]),
+	CONSTRAINT FK_Subtask_UserTask FOREIGN KEY ([UserTaskId]) REFERENCES [UserTask]([Id]) ON DELETE SET NULL
 )
