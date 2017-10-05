638213df83b1a0a7f885f877dec8887a347af03f
Maximsan <maximuzz@yandex.ru>
2017-10-05 17:34:03 +0300

    Modified Phone, TestType tables

--- HIMS.Database/HIMS.Database/Tables/Phone.sql
+++ HIMS.Database/HIMS.Database/Tables/Phone.sql
@@ -1,8 +1,10 @@
 ﻿CREATE TABLE [dbo].[Phone]
 (
-	[Id] INT NOT NULL, 
+	[Id] INT IDENTITY(1,1) NOT NULL, 
     [MobilePhone] NVARCHAR(15) NOT NULL, 
-    [Skype] NCHAR(30) NULL  -- if have no Skype account yet
+    [Skype] NVARCHAR(30) NULL  -- if have no Skype account yet
 
-	CONSTRAINT [PK_Phone] PRIMARY KEY ([Id])
+	CONSTRAINT PK_Phone PRIMARY KEY ([Id]),
+	CONSTRAINT UQ_MobilePhone UNIQUE ([MobilePhone]),
+	CONSTRAINT UQ_Skype UNIQUE ([Skype])
 )
