fdef538b94cdf4c15fac15c61cc0b9707e4b5d64
Maximsan <maximuzz@yandex.ru>
2017-10-05 17:18:36 +0300

    Modified Task table

--- HIMS.Database/HIMS.Database/Tables/Task.sql
+++ HIMS.Database/HIMS.Database/Tables/Task.sql
@@ -3,11 +3,10 @@
 	[Id] INT IDENTITY (1,1) NOT NULL, 
     [Name] NVARCHAR(25) NOT NULL, 
     [Description] NVARCHAR(255) NOT NULL, 
+	[StartDate] DATETIME NOT NULL,
+	[Deadline] DATETIME NULL DEFAULT NULL, 	--DEFAULT NULL means "no deadline"
     [TimeStamp] TIMESTAMP NOT NULL, 
-    [Deadline] DATETIME NULL DEFAULT NULL, 
-	--DEFAULT NULL means "no deadline"
-    [StartDate] DATETIME NOT NULL,
 
-	CONSTRAINT [PK_Task] PRIMARY KEY ([Id])
+	CONSTRAINT PK_Task PRIMARY KEY ([Id])
 )
 
